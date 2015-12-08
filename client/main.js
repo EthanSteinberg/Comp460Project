788caf457444b2ecc9ab0145614d9070e27d38a8
wruths <weston@ruthsteam.org>
2015-12-08 00:18:47 -0600

    Canvas and rectangle added

--- client/main.js
+++ client/main.js
@@ -1,3 +1,16 @@
 var foo = require('./foo.js');
 
 console.log("Hello! " + foo);
+
+function draw() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillRect(25,25,100,100);
+    ctx.clearRect(45,45,60,60);
+    ctx.strokeRect(50,50,50,50);
+  }
+}
+
+window.draw = draw;
