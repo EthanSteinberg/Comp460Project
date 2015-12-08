97ebabf1c2c17af4f798362b08a02461d4157f96
Ethan Steinberg <ethan.steinberg@gmail.com>
2015-12-07 23:34:50 -0600

    Current

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/static/index.html');
 });
+
+app.use('/static', express.static('client'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
