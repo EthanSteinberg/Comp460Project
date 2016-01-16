1a90c6748226f65d90358316c22bbddff07b3042
wruths <weston@ruthsteam.org>
2016-01-15 19:12:08 -0600

    Incorporated the main class

--- app/client/main.js
+++ app/client/main.js
@@ -14,6 +14,130 @@ const SHIPBUILDMODE = true;
 /**
  * The central game object for most of the logic.
  */
+class Main {
+  /**
+   * Contruct the game.
+   * Takes as input the images object as produced by images.js
+   */
+  constructor(images) {
+    this.mode = 'game';
+
+    this.images = images;
+    this.game = new Game(images);
+    const shipbuilder = new Shipbuilder(images);
+
+    this.canvas = document.getElementById('canvas');
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.context = this.canvas.getContext('2d');
+
+    this.pressedKeys = new Set();
+
+    this.actionMap = {
+      37: () => this.game.x -= 1,
+      38: () => this.game.y -= 1,
+      39: () => this.game.x += 1,
+      40: () => this.game.y += 1,
+    };
+
+    document.addEventListener('keydown', (event) => {
+      this.game.keydown(event, this.pressedKeys);
+    });
+
+    document.addEventListener('keyup', (event) => {
+      this.game.keyup(event, this.pressedKeys);
+    });
+
+    // Ignore right click events on the canvas
+    this.canvas.addEventListener('contextmenu', (event) => {
+      event.preventDefault();
+    });
+
+    this.canvas.addEventListener('mousedown', (event) => {
+      if (this.mode == 'game') {
+        this.game.mousedown(event, this.sendMessage.bind(this));
+      } else if (this.mode == 'shipbuilder') {
+        this.shipbuilder.mousedown(event);
+      }
+    });
+
+    this.messageHandlerMap = {
+      'SetShipPosition': this._setShipPositionHandler.bind(this),
+      'SetPosition': this._setPositionHandler.bind(this),
+    };
+  }
+
+  _setShipPositionHandler(setShipPositionMessage) {
+    this.game._setShipPositionHandler(setShipPositionMessage);
+  }
+
+  _setPositionHandler(setPositionMessage) {
+    this.game._setPositionHandler(setPositionMessage);
+  }
+
+  sendMessage(message) {
+    console.log('Sending', message);
+    this.ws.send(JSON.stringify(message));
+  }
+
+  /**
+   * Start the game by setting up the render intervals.
+   */
+  start() {
+    this.ws = new WebSocket('ws://localhost:3000');
+    this.ws.onmessage = this._onMessage.bind(this);
+
+    setInterval(this.render.bind(this), MILLISECONDS_PER_RENDER_UPDATE);
+    this.lastUpdate = performance.now();
+
+    this.start = performance.now();
+  }
+
+  _onMessage(event) {
+    const messageData = JSON.parse(event.data);
+    if (messageData.type in this.messageHandlerMap) {
+      this.messageHandlerMap[messageData.type](messageData);
+    }
+    console.log('Got' + event.data);
+  }
+
+  /**
+   * Update the game state when necessary.
+   */
+  update(currentTime) {
+    while (currentTime > this.lastUpdate) {
+      this.tick();
+      this.lastUpdate += MILLISECONDS_PER_LOGIC_UPDATE;
+    }
+  }
+
+  /**
+   * Perform a discrete update of the logic.
+   */
+  tick() {
+    for (const key in this.actionMap) {
+      if (this.pressedKeys.has(+key)) {
+        this.actionMap[key]();
+      }
+    }
+  }
+
+  /**
+   * Render the game. Also performs updates if necessary.
+   */
+  render() {
+    const time = performance.now();
+
+    this.update(time);
+
+    if (this.mode == 'game') {
+      this.game.render();
+    } else if (this.mode == 'shipbuilder') {
+      this.shipbuilder.render();
+    }
+  }
+}
+
 class Game {
   /**
    * Contruct the game.
@@ -24,52 +148,33 @@ class Game {
     this.map = new GameMap();
     this.gui = new Gui();
 
+    this.x = 0;
+    this.y = 0;
+
     this.canvas = document.getElementById('canvas');
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.context = this.canvas.getContext('2d');
+  }
 
-    // Ignore right click events on the canvas
-    this.canvas.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-    });
-
-    this.canvas.addEventListener('mousedown', (event) => {
-      if (event.button === 2) {
-        // Deselect on right click
-        if (this.selectedItem instanceof Shipyard) {
-          this.gui.removeShipyardDisplay();
-        }
-        this.setSelectedItem(null);
-      } else if (event.button === 0) {
-        const rect = this.canvas.getBoundingClientRect();
-        const rawX = event.clientX - rect.left;
-        const rawY = event.clientY - rect.top;
-
-        if (rawX > 400) {
-          this.processGuiMouseClick(rawX, rawY);
-        } else {
-          this.processMapMouseClick(rawX, rawY);
-        }
+  mousedown(event, sendMessage) {
+    if (event.button === 2) {
+      // Deselect on right click
+      if (this.selectedItem instanceof Shipyard) {
+        this.gui.removeShipyardDisplay();
       }
-    });
+      this.setSelectedItem(null);
+    } else if (event.button === 0) {
+      const rect = this.canvas.getBoundingClientRect();
+      const rawX = event.clientX - rect.left;
+      const rawY = event.clientY - rect.top;
 
-    this.pressedKeys = new Set();
-
-    this.x = 0;
-    this.y = 0;
-
-    this.actionMap = {
-      37: () => this.x -= 1,
-      38: () => this.y -= 1,
-      39: () => this.x += 1,
-      40: () => this.y += 1,
-    };
-
-    this.messageHandlerMap = {
-      'SetShipPosition': this._setShipPositionHandler.bind(this),
-      'SetPosition': this._setPositionHandler.bind(this),
-    };
+      if (rawX > 400) {
+        this.processGuiMouseClick(rawX, rawY);
+      } else {
+        this.processMapMouseClick(rawX, rawY, sendMessage);
+      }
+    }
   }
 
   processGuiMouseClick(rawX, rawY) {
@@ -81,7 +186,7 @@ class Game {
     }
   }
 
-  processMapMouseClick(rawX, rawY) {
+  processMapMouseClick(rawX, rawY, sendMessage) {
     const x = rawX + this.x - 25;
     const y = rawY + this.y - 25;
 
@@ -100,18 +205,18 @@ class Game {
         if (this.guiSelected) {
           // If an empty tile on an island is selected then add a building
           if (this.selectedItem.getType() == 'shiptemplate') {
-            this.sendMessage({ type: 'MakeShip', islandID: this.selectedShipyard.getIslandID(), x: mouseRoundedX, y: mouseRoundedY });
+            sendMessage({ type: 'MakeShip', islandID: this.selectedShipyard.getIslandID(), x: mouseRoundedX, y: mouseRoundedY });
             this.gui.removeShipyardDisplay();
           } else {
             var buildingType = this.selectedItem.getType();
-            this.sendMessage({ type: 'MakeBuilding', building: buildingType, x: mouseRoundedX, y: mouseRoundedY });
+            sendMessage({ type: 'MakeBuilding', building: buildingType, x: mouseRoundedX, y: mouseRoundedY });
           }      
           this.guiSelected = false;
           this.setSelectedItem(item); 
         } else if (this.selectedItem instanceof Ship) {
           // Try to move to that location.
           const targetLocation = { x: mouseX, y: mouseY };
-          this.sendMessage({ type: 'MoveShip', shipId: this.selectedItem.getId(), targetLocation });
+          sendMessage({ type: 'MoveShip', shipId: this.selectedItem.getId(), targetLocation });
         } 
       } else {
         // TODO: Add logic for attacking stuff.
@@ -158,64 +263,10 @@ class Game {
     this.map.addBuilding(object, position.x, position.y, islandID);
   }
 
-  sendMessage(message) {
-    console.log('Sending', message);
-    this.ws.send(JSON.stringify(message));
-  }
-
-  /**
-   * Start the game by setting up the render intervals.
-   */
-  start() {
-    this.ws = new WebSocket('ws://localhost:3000');
-    this.ws.onmessage = this._onMessage.bind(this);
-
-    setInterval(this.render.bind(this), MILLISECONDS_PER_RENDER_UPDATE);
-    this.lastUpdate = performance.now();
-
-    this.frames = 0;
-    this.start = performance.now();
-  }
-
-  _onMessage(event) {
-    const messageData = JSON.parse(event.data);
-    if (messageData.type in this.messageHandlerMap) {
-      this.messageHandlerMap[messageData.type](messageData);
-    }
-    console.log('Got' + event.data);
-  }
-
-  /**
-   * Update the game state when necessary.
-   */
-  update(currentTime) {
-    while (currentTime > this.lastUpdate) {
-      this.tick();
-      this.lastUpdate += MILLISECONDS_PER_LOGIC_UPDATE;
-    }
-  }
-
-  /**
-   * Perform a discrete update of the logic.
-   */
-  tick() {
-    for (const key in this.actionMap) {
-      if (this.pressedKeys.has(+key)) {
-        this.actionMap[key]();
-      }
-    }
-  }
-
   /**
    * Render the game. Also performs updates if necessary.
    */
   render() {
-    const time = performance.now();
-
-    this.frames += 1;
-
-    this.update(time);
-
     this.context.clearRect(0, 0, this.width, this.height);
 
     this.context.save();
@@ -230,23 +281,14 @@ class Game {
     this.context.restore();
      // Render the gui
     this.gui.render(this.context, this.images);
-
-    this.context.font = '50px sans-serif';
-    this.context.fillStyle = 'black';
-    this.context.fillText('FPS: ' + this.frames / (time - this.start) * 1000, 0, 400);
-
-    if (this.frames === 100) {
-      this.start = time;
-      this.frames = 0;
-    }
   }
 
-  keydown(event) {
-    this.pressedKeys.add(event.keyCode);
+  keydown(event, pressedKeys) {
+    pressedKeys.add(event.keyCode);
   }
 
-  keyup(event) {
-    this.pressedKeys.delete(event.keyCode);
+  keyup(event, pressedKeys) {
+    pressedKeys.delete(event.keyCode);
   }
 }
 
@@ -257,83 +299,24 @@ class Shipbuilder {
   constructor(images) {
     this.images = images;
     this.shipbuildergui = new ShipbuilderGui();
+
     this.canvas = document.getElementById('canvas');
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.context = this.canvas.getContext('2d');
-
-    // Ignore right click events on the canvas
-    this.canvas.addEventListener('contextmenu', (event) => {
-      event.preventDefault();
-    });
-
-    this.canvas.addEventListener('mousedown', (event) => {
-      const rect = this.canvas.getBoundingClientRect();
-      const mouseX = event.clientX - rect.left + this.x - 25;
-      const mouseY = event.clientY - rect.top + this.y - 25;
-
-      if (event.button === 2) {
-        // Deselect on right click
-        this.shipbuildergui.deselect();
-        this.shipbuildergui.emptyslot(mouseX, mouseY);
-      } else if (event.button === 0) {
-        this.shipbuildergui.select(mouseX, mouseY);
-      }
-    });
-
-    this.pressedKeys = new Set();
-
-    this.x = 0;
-    this.y = 0;
-
-    this.actionMap = {
-      37: () => this.x -= 1,
-      38: () => this.y -= 1,
-      39: () => this.x += 1,
-      40: () => this.y += 1,
-    };
   }
 
-  /**
-   * Start the game by setting up the render intervals.
-   */
-  start() {
-    // this.ws = new WebSocket('ws://localhost:3000');
-    // this.ws.onmessage = this._onMessage.bind(this);
+  mousedown(event) {
+    const rect = this.canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left + this.x - 25;
+    const mouseY = event.clientY - rect.top + this.y - 25;
 
-    setInterval(this.render.bind(this), MILLISECONDS_PER_RENDER_UPDATE);
-    this.lastUpdate = performance.now();
-
-    this.frames = 0;
-    this.start = performance.now();
-  }
-
-  _onMessage(event) {
-    const messageData = JSON.parse(event.data);
-    if (messageData.type in this.messageHandlerMap) {
-      this.messageHandlerMap[messageData.type](messageData);
-    }
-    console.log('Got' + event.data);
-  }
-
-  /**
-   * Update the game state when necessary.
-   */
-  update(currentTime) {
-    while (currentTime > this.lastUpdate) {
-      this.tick();
-      this.lastUpdate += MILLISECONDS_PER_LOGIC_UPDATE;
-    }
-  }
-
-  /**
-   * Perform a discrete update of the logic.
-   */
-  tick() {
-    for (const key in this.actionMap) {
-      if (this.pressedKeys.has(+key)) {
-        this.actionMap[key]();
-      }
+    if (event.button === 2) {
+      // Deselect on right click
+      this.shipbuildergui.deselect();
+      this.shipbuildergui.emptyslot(mouseX, mouseY);
+    } else if (event.button === 0) {
+      this.shipbuildergui.select(mouseX, mouseY);
     }
   }
 
@@ -341,12 +324,6 @@ class Shipbuilder {
    * Render the game. Also performs updates if necessary.
    */
   render() {
-    const time = performance.now();
-
-    this.frames += 1;
-
-    this.update(time);
-
     this.context.clearRect(0, 0, this.width, this.height);
     this.context.save();
 
@@ -358,29 +335,12 @@ class Shipbuilder {
     this.shipbuildergui.render(this.context, this.images);
 
     this.context.restore();
-
-    if (this.frames === 100) {
-      this.start = time;
-      this.frames = 0;
-    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', function startCanvas() {
   loadImages().then((images) => {
-    const game = new Game(images);
-
-    document.addEventListener('keydown', (event) => {
-      game.keydown(event);
-    });
-
-    document.addEventListener('keyup', (event) => {
-      game.keyup(event);
-    });
-
-    // game.start();
-
-    const shipbuilder = new Shipbuilder(images);
-    shipbuilder.start();
+    const main = new Main(images);
+    main.start();
   });
 });
